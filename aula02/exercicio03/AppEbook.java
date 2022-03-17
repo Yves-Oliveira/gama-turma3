631d106a1c0c202bbd7851809ebba37328912979
Yves-Oliveira <contact@yvesoliveira.com>
2022-03-17 14:27:25 -0300

    exercicio 4

---
+++ aula02/exercicio03/AppEbook.java
@@ -0,0 +1,22 @@
+package exercicio03;
+
+public class AppEbook {
+    public static void main(String[] args) {
+        Ebook livro = new Ebook("Programar em Java", "Deitei", 300);
+
+        System.out.println(livro.mostrarDados());
+        ;
+        livro.avancarPagina();
+        System.out.println(livro.getPaginaAtual());
+        livro.voltarPagina();
+        livro.voltarPagina();
+        System.out.println(livro.getPaginaAtual());
+
+        boolean alterou = livro.irParaPagina(400);
+        if (alterou) {
+            System.out.println("Foi para a página");
+        } else {
+            System.out.println("Página inválida");
+        }
+    }
+}
