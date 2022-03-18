34bfdcf17cd545431df9de4778ae51d3533aa59f
Yves-Oliveira <contact@yvesoliveira.com>
2022-03-18 16:09:25 -0300

    Aula 04

--- aula04/projed/AppED.java
+++ aula04/projed/AppED.java
@@ -1,7 +1,24 @@
 package projed;
 
+import java.lang.ref.Cleaner;
+import java.util.ArrayList;
+
 public class AppED {
     public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<>();
 
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+            if (lista.get(i) == 20) {
+                System.out.println("Achei o número desejado.");
+                break;
+            }
+        }
+        System.out.println("Terminou.");
+        Clrscr();
     }
 }
